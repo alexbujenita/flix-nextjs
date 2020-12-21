7ec9a68844d49e14112b0c8195bb37211539b4ee
alexbujenita <dekc89@gmail.com>
2020-12-21 16:21:29 +0000

    basic movie card

--- src/pages/movies/index.js
+++ src/pages/movies/index.js
@@ -1,18 +1,28 @@
 import axios from "axios";
-
+import MovieCard from "../components/NavBar/MovieCard/MovieCard";
+import styles from './Movies.module.scss'
 export default function Movies(props) {
-  console.log(props);
+  // console.log(props);
   return (
-    <div>
+    <>
       <h1>MOVIES!</h1>
-    </div>
+      <div className={styles.moviesContainer}>
+        {props.results.map((movie) => {
+          return <MovieCard key={movie.id} {...movie} />;
+        })}
+      </div>
+    </>
   );
 }
 
 export async function getServerSideProps(ctx) {
-  const { query: { page = 1 } } = ctx;
+  const {
+    query: { page = 1 },
+  } = ctx;
   try {
-    const { data } = await axios.get(`http://localhost:3001/api/movies?page=${page}`);
+    const { data } = await axios.get(
+      `http://localhost:3001/api/movies?page=${page}`
+    );
     return {
       props: data,
     };
