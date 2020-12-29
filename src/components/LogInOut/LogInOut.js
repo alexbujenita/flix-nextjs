a039e03f2a262ee182911dccc3571891d49d9ae7
alexbujenita <dekc89@gmail.com>
2020-12-29 17:50:03 +0000

    removing cookie on logout from server

--- src/components/LogInOut/LogInOut.js
+++ src/components/LogInOut/LogInOut.js
@@ -1,26 +1,36 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
+import axios from "axios";
 import styles from "./LogInOut.module.scss";
 
 export default function LogInOut({ name }) {
-
   const router = useRouter();
-  function clearLocalStorage() {
-    localStorage.clear("LOGGED");
-    localStorage.clear("UserFavs");
-    router.push("/movies");
+  async function logoutUser() {
+    try {
+      await axios.delete("http://localhost:3001/api/auth/logout", {
+        withCredentials: true,
+      });
+      localStorage.clear("LOGGED");
+      localStorage.clear("UserFavs");
+      router.push("/movies");
+    } catch (error) {
+      console.log(error);
+      alert("Retry.");
+    }
   }
 
   return name ? (
-    <h3 onClick={clearLocalStorage}>LOGOUT</h3>
+    <h3 className={styles.elements} onClick={logoutUser}>
+      LOGOUT
+    </h3>
   ) : (
     <div className={styles.logOrRegister}>
       <Link href="/login">
-        <h3>LOGIN</h3>
+        <h3 className={styles.elements}>LOGIN</h3>
       </Link>
       <h2>&nbsp; / &nbsp;</h2>
       <Link href="/register">
-        <h3>REGISTER</h3>
+        <h3 className={styles.elements}>REGISTER</h3>
       </Link>
     </div>
   );
