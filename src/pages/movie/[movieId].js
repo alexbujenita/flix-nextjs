11b575c85b3e6a403b390ef8d5cf8005d94fa444
alexbujenita <dekc89@gmail.com>
2021-01-14 19:34:06 +0000

    image on movie page

--- src/pages/movie/[movieId].js
+++ src/pages/movie/[movieId].js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import Head from "next/head";
+import Image from "next/image";
 import { useState } from "react";
 import AddRemoveFav from "../../components/AddRemoveFav/AddRemoveFav";
 import PersonCard from "../../components/PersonCard/PersonCard";
@@ -9,7 +10,7 @@ import styles from "./Movie.module.scss";
 export default function Movie(props) {
   const { credits, movie, trailers } = props;
   const [displayCast, setDisplayCast] = useState(false);
-
+  const { poster_path, title, original_title, tagline, overview } = movie;
 
   return (
     <>
@@ -23,28 +24,36 @@ export default function Movie(props) {
         />
       </Head>
       <div className={styles.movieIntroContainer}>
+        {poster_path ? (
+          <Image
+            src={`https://image.tmdb.org/t/p/w185${poster_path}`}
+            alt={title}
+            width={185}
+            height={278}
+          />
+        ) : null}
         <div className={styles.movieIntro}>
-          <h1>{movie.title || movie.original_title}</h1>
-          {movie.tagline ? (
+          <h1>{title || original_title}</h1>
+          {tagline ? (
             <h2>
-              <i>{movie.tagline}</i>
+              <i>{tagline}</i>
             </h2>
           ) : null}
-          {movie.overview ? <h3>{movie.overview}</h3> : null}
+          {overview ? <h3>{overview}</h3> : null}
         </div>
       </div>
       <AddRemoveFav movie={movie} />
       {trailers?.results?.length ? (
         <Trailers trailers={trailers.results} />
       ) : null}
-      {credits?.cast?.length && !displayCast && (
+      {credits?.cast?.length && !displayCast ? (
         <h2
           className={styles.showHideCast}
           onClick={() => setDisplayCast(true)}
         >
           SHOW CAST
         </h2>
-      )}
+      ) : null}
       {displayCast && (
         <h2
           className={styles.showHideCast}
